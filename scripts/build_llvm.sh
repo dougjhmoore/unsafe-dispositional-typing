950fc25182b3e2fb822cf0b755613b129625252c
Doug J. H. Moore <djhmoore@gmail.com>
2025-05-28 10:01:51 +1000

    build_llvm.sh: use -mllvm=-passes=dispositional-pass syntax

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -18,10 +18,10 @@ cmake -G Ninja "$SRC_DIR" \
   -DCMAKE_CXX_COMPILER=clang++-17 \
   -DCMAKE_C_FLAGS="\
       -fpass-plugin=$PLUGIN \
-      -mllvm -passes=dispositional-pass" \
+      -mllvm=-passes=dispositional-pass" \
   -DCMAKE_CXX_FLAGS="\
       -fpass-plugin=$PLUGIN \
-      -mllvm -passes=dispositional-pass"
+      -mllvm=-passes=dispositional-pass"
 
 # 3. build & capture log -----------------------------------------------------
 ninja | tee /data/benchmarks/llvm/llvm_raw.log
